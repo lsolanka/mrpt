9c3c4e821aa050966670fa0bb96d5bd05afc304b
Lukas Solanka <lsolanka@gmail.com>
2019-04-13 13:10:00 +0200

    add jsoncpp through Hunter

--- cmakemodules/script_jsoncpp.cmake
+++ cmakemodules/script_jsoncpp.cmake
@@ -6,7 +6,7 @@ option(DISABLE_JSONCPP "Forces NOT using JSONCPP, even if it could be found by C
 mark_as_advanced(DISABLE_JSONCPP)
 
 if(NOT DISABLE_JSONCPP)
-	find_package(jsoncpp QUIET)
+	find_package(jsoncpp CONFIG REQUIRED)
 	if(jsoncpp_FOUND)
 		# cmake imported target: "jsoncpp_lib"
 		set(CMAKE_MRPT_HAS_JSONCPP 1)
